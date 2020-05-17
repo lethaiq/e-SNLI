ebd6ec1a662813bce7ee0ffed0d911a2297625e6
thai le <thai.le@ist.psu.edu>
2020-05-17 14:03:09 -0400

    updates

--- attention/data_attention_bottom.py
+++ attention/data_attention_bottom.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 import shutil
 import sys
-sys.path.add('../utils')
+sys.path.append("..")
 from mutils import makedirs
 
 NLI_DIC_LABELS = {'entailment': 2,  'neutral': 1, 'contradiction': 0}
